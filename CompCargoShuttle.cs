027819ae2d4e700b763723f5e538a51523cffb7d
Boarlock <derick.clark.jr1998@gmail.com>
2026-09-16 18:27:44 -0400

    Initial commit

---
+++ CompCargoShuttle.cs
@@ -0,0 +1,83 @@
+﻿using DubsBadHygiene;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace CargoShuttle
+{
+    public class CompProperties_CargoShuttle : CompProperties
+    {
+        public CompProperties_CargoShuttle()
+        {
+            compClass = typeof(CompCargoShuttle);
+        }
+    }
+
+    public class CompCargoShuttle : ThingComp
+    {
+        public PlumbingNet? AdjacentPlumbingNet;
+        private float waterStorage = 500f;
+        private float waterCapacity = 500f;
+
+        public float WaterStorage
+        {
+            get
+            {
+                return waterStorage;
+            }
+            set
+            {
+                waterStorage = value;
+            }
+        }
+        public float WaterCapacity
+        {
+            get
+            {
+                return waterCapacity;
+            }
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            Log.Message("CARGO SHUTTLE CompGetGizmosExtra CALLED");
+
+            foreach (Gizmo gizmo in base.CompGetGizmosExtra())
+                yield return gizmo;
+
+            Log.Message("CARGO SHUTTLE ABOUT TO YIELD WATER GIZMO");
+
+            yield return new Command_Action
+            {
+                defaultLabel = "Discharge",
+                defaultDesc = "Unload water into the adjacent DBH plumbing network.",
+                icon = ContentFinder<Texture2D>.Get("UI/Gizmo/UnloadWater"),
+                Disabled = waterStorage <= 0f || AdjacentPlumbingNet == null,
+                action = () => ShuttleWater.UnloadWater(parent)
+            };
+            
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            return $"Stored Water: {waterStorage:0.##} / {waterCapacity:0.##}";
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+
+            if (parent.IsHashIntervalTick(180))
+            {
+                AdjacentPlumbingNet = ShuttleWater.CheckCellsAroundShuttle(parent);
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            Scribe_Values.Look(ref waterStorage, "waterStorage", 0f);
+        }
+    }
+}
---
+++ ShuttleWater.cs
@@ -0,0 +1,91 @@
+﻿using DubsBadHygiene;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace CargoShuttle
+{
+    public class ShuttleWater
+    {
+        public static PlumbingNet? CheckCellsAroundShuttle(Thing shuttle)
+        {
+            if (shuttle == null)
+                return null;
+
+            CompCargoShuttle shuttleComp = shuttle.TryGetComp<CompCargoShuttle>();
+
+            if (shuttleComp == null)
+                return null;
+
+            // Make sure the shuttle is on a non-null worldspace currently.
+            Map map = shuttle.Map;
+
+            if (map == null)
+                return null;
+
+            HashSet<IntVec3> adjacentTilesSet = new HashSet<IntVec3>();
+
+            // Get the cells adjacent to the shuttle.
+            foreach (IntVec3 shuttleCell in shuttle.OccupiedRect())
+            {
+                foreach (IntVec3 adjCell in GenAdjFast.AdjacentCells8Way(shuttleCell))
+                {
+                    adjacentTilesSet.Add(adjCell);
+                }
+            }
+
+            // Remove the shuttle cells themselves from adjacent cell list.
+            foreach (IntVec3 shuttleCell in shuttle.OccupiedRect())
+            {
+                adjacentTilesSet.Remove(shuttleCell);
+            }
+
+            PlumbingNet net = null!;
+
+            foreach (IntVec3 adjTile in adjacentTilesSet)
+            {
+                if (!adjTile.InBounds(map))
+                    continue;
+
+                foreach (Thing thing in map.thingGrid.ThingsAt(adjTile))
+                {
+                    CompPipe pipe = thing.TryGetComp<CompPipe>();
+
+                    if (pipe == null)
+                        continue;
+
+                    net = pipe.pipeNet;
+                    break;
+                }
+
+                if (net != null)
+                    return net;
+            }
+            return null;
+        }
+
+        public static void UnloadWater(Thing shuttle)
+        {
+            if (shuttle == null)
+                return;
+
+            CompCargoShuttle shuttleComp = shuttle.TryGetComp<CompCargoShuttle>();
+
+            if (shuttleComp == null || shuttleComp.AdjacentPlumbingNet == null)
+                return;
+
+            float amountToTransfer = shuttleComp.WaterStorage;
+
+            if (amountToTransfer <= 0f)
+                return;
+
+            float remaining = shuttleComp.AdjacentPlumbingNet.PushWater(amountToTransfer);
+            float transferred = amountToTransfer - remaining;
+
+            if (transferred <= 0f)
+                return;
+
+            shuttleComp.WaterStorage -= transferred;
+        }
+    }
+}
